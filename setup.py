7efde884499ee77848c48ccc79e3917c8d655aa3
CTO <cto@filterfeed.com.br>
2023-05-25 18:20:01 -0300

    Initial commit: Add Ploomes API client with setup.py and README

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ploomes-api-client',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),   
     url='https://github.com/victorfigueredo/ploomes-api-client',
     author='Victor Figueredo',
